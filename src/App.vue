10c98f953edb2b779ca3929a85a6bea6521ab38e
Ariana <74054656+VincitQuiPatitur@users.noreply.github.com>
2023-08-25 10:51:21 +0300

    feat: create a list with the ability to add new items

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,48 @@
+<script>
+import PostForm from "@/components/PostForm.vue";
+import PostList from "@/components/PostList.vue";
+export default {
+  components: {
+    PostForm, PostList
+  },
+  data() {
+    return {
+      posts: [
+        {id: 1, title: 'Пост о JavaScrip 1', body: 'JavaScript - универсальный язык программирования 1'},
+        {id: 2, title: 'Пост о JavaScrip 2', body: 'JavaScript - универсальный язык программирования 2'},
+        {id: 3, title: 'Пост о JavaScrip 3', body: 'JavaScript - универсальный язык программирования 3'},
+      ],
+    }
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post);
+    },
+  }
+}
+</script>
+<!-- Полная запись:  v-on:click  v-bind:posts -->
+<!-- Короткая запись:    @click        :posts -->
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <post-form
+        @create="createPost"
+    />
+    <post-list
+        :posts="posts"
+    />
+  </div>
 </template>
 
+<!-- scoped говорит о применимости стилей только к этому компоненту и недопустимости извне-->
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.app {
+  padding: 20px;
 }
 </style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,73 @@
+<script>
+
+export default {
+  data() {
+    return {
+      post: {
+        title: '',
+        body: ''
+      },
+    }
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      // передача значений от дочернего элемента родительскому
+      this.$emit('create', this.post);
+      this.post = {
+        title: '',
+        body: ''
+      };
+    }
+  }
+}
+
+</script>
+
+<template>
+  <form class="form" @submit.prevent>
+    <h4>Создание поста</h4>
+    <!--  v-bind:value="post.title"
+          @input="post.title = $event.target.value"
+    -->
+    <input
+        v-model="post.title"
+        class="input"
+        type="text"
+        placeholder="Название"
+    >
+    <input
+        v-model="post.body"
+        class="input"
+        type="text"
+        placeholder="Описание"
+    >
+    <button
+        @click="createPost"
+        class="btn"
+    >Опубликовать</button>
+  </form>
+</template>
+
+<style scoped>
+.form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn {
+  align-self: flex-end;
+  margin-top: 15px;
+  padding: 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+</style>
